fa9119531854d492c3d0943a80ce22a7a1723ea1
José Antonio Henríquez Chavarría <alphyon21@yahoo.es>
2017-06-12 22:42:59 -0600

    paso5 - Creando Relaciones entre modelos

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,44 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Seller;
+use App\Product;
+use App\Category;
+use App\Transaction;
 
 class Product extends Model
 {
-    //
+  const PRODUCT_AVALAIBLE = 'avalaible';
+  const PRODUCT_NOT_AVALAIBLE = 'not avalaible';
+
+    protected $fillable = [
+      'name',
+      'description',
+      'quantity',
+      'status',
+      'image',
+      'seller_id'
+    ];
+
+    public function isAvailable()
+    {
+      return $this->status == Product::PRODUCT_AVALAIBLE;
+    }
+
+    public function seller()
+    {
+      return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions()
+    {
+      return $this->hasMany(Transaction::class);
+    }
+
+    public function categories()
+    {
+      return $this->belongsToMany(Category::class);
+    }
+
+
 }
--- app/User.php
+++ app/User.php
@@ -10,13 +10,25 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const USER_VERIFY = '1';
+    const USER_NOT_VERIFY = '0';
+    const USER_ADMINISTRATOR = 'true';
+    const USER_NOT_ADMINISTRATOR = 'false';
+
+
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin'
     ];
 
     /**
@@ -25,6 +37,23 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verification_token',
     ];
+
+    public function isVerify()
+    {
+      return $this->verified == User::USER_VERIFY;
+    }
+
+    public function isAdministrator()
+    {
+      return $this->admin == User::USER_ADMINISTRATOR;
+    }
+
+    public static function generateVarificationToken()
+    {
+      return str_random(40);
+    }
 }
